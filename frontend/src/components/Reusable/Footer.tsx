29893110189ba371d72179ed4202aac9cba30b59
Olya <balagush.olga01@gmail.com>
2024-04-15 20:57:30 +0300

    footer

---
+++ frontend/src/components/Reusable/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+
+    return (
+        <footer className="bg-custom-dark text-white px-12 py-3 absolute bottom-0 w-full">
+            footer
+        </footer>
+    )
+}
+
+export default Footer;
