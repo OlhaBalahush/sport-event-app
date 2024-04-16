909cd237bc1839d733c62e3b5932700aa23d29a4
Olya <balagush.olga01@gmail.com>
2024-04-16 17:22:10 +0300

    svg fixes

--- frontend/src/components/assets/WaterSports.tsx
+++ frontend/src/components/assets/WaterSports.tsx
@@ -1,6 +1,6 @@
 const WaterSports = () => {
     return (
-        <svg width="88" height="62" viewBox="0 0 88 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className="w-[70%] h-[70%]" width="88" height="62" viewBox="0 0 88 62" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M1 38C5.5 38 13.5 47.5 17.5 47.5C21.5 47.5 30 38 36 38C42 38 47 47.5 52 47.5C57 47.5 64 38 70.5 38C77 38 82 47.5 87 47.5" stroke="black" strokeWidth="2" strokeLinecap="round" />
             <path d="M1 51C5.5 51 13.5 60.5 17.5 60.5C21.5 60.5 30 51 36 51C42 51 47 60.5 52 60.5C57 60.5 64 51 70.5 51C77 51 82 60.5 87 60.5" stroke="black" strokeWidth="2" strokeLinecap="round" />
             <path d="M15.9999 37C22.1666 32.5 34.6999 23.3 35.4999 22.5C36.4999 21.5 25.9999 10.5 24.4999 10.5C22.9999 10.5 12.0883 17.6078 11.5 18C10.0001 19 7.33326 21 3.99992 16C1.99991 13 10.4999 8.5 14.4999 6C18.4999 3.5 23.9999 1 25 1C27.5001 1 39.7404 10.2404 43 13.5C48 18.5 55 28 55.5 36" stroke="black" strokeWidth="2" strokeLinecap="round" />
