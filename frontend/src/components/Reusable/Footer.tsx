3816ff4771193f877d84b00bcfb74cfb9a994e76
Olya <balagush.olga01@gmail.com>
2024-05-01 13:32:31 +0300

    footer

--- frontend/src/components/Reusable/Footer.tsx
+++ frontend/src/components/Reusable/Footer.tsx
@@ -1,8 +1,11 @@
 const Footer = () => {
 
     return (
-        <footer className="bg-custom-dark text-white px-12 xl:px-40 py-3 absolute bottom-0 w-full">
-            footer
+        <footer className="flex flex-row gap-5 bg-custom-dark text-white px-12 xl:px-40 py-3 absolute bottom-0 w-full">
+            © 2024 SpartEventsApp. All rights reserved.
+            <a href="https://github.com/OlhaBalahush/sport-event-app" target="_blank" className="hover:text-custom-light-blue">
+                GitHub Repository
+            </a>
         </footer>
     )
 }
